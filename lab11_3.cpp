f8e9ae49f316fe30fd959f46003daede9eb54e00
suphakrit <suphakrit_kongkham@cmu.ac.th>
2023-01-03 16:04:09 +0700

    Fantaasticcc

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    
+    ifstream source;
+    string textline;
+    double sum;
+    double avgp2;
+    double sump2;
+    int N;
+    source.open("score.txt");
+    while(getline(source, textline)){
+        sum += stof(textline);
+        sump2 += pow(stof(textline),2);       
+        N++;
+        
+    }
+    avgp2 = pow(sum/N, 2);
+
+    cout << "Number of data = " << N<<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<< sum/N<<"\n";
+    cout << "Standard deviation = "<< pow((sump2/N)-avgp2, 0.5)<<"\n";
+}
