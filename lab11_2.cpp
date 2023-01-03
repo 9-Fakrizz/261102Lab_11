1a5da6bd3d91a1d734f9697d1ee5e3a41726e103
suphakrit <suphakrit_kongkham@cmu.ac.th>
2023-01-03 15:33:52 +0700

    Gotchaa

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -6,9 +6,17 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
+	string textline;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
-	
+	dest << "-------------------- BOOM ---------------------\n";
+	cout << "-------------------- BOOM ---------------------\n";
+	while(getline(source, textline)){
+		dest << textline <<"\n";
+		cout << textline <<"\n";
+	}
+	dest << "-------------------- HA!! ---------------------";
+	cout << "-------------------- HA!! ---------------------";
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
